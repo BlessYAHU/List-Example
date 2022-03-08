ca13e986d10ec6492e060f0b9b4c488025cc57ce
A'braham Barakhyahu <barakhyahu@gmail.com>
2022-03-08 18:32:56 -0500

    initial commit

--- src/components/list/ListItems.ts
+++ src/components/list/ListItems.ts
@@ -1,7 +1,8 @@
 interface ListItemCollection {
   addItem: (item: string) => void;
   getItems: () => string[];
-  removeItem: (itemIndexToRemove: number) => removeItemResults;
+  removeItem: (itemIndexToRemove: number) => void;
+  undoRemoveLastItem: () => void;
 }
 
 interface removeItemResults {
@@ -11,22 +12,26 @@ interface removeItemResults {
 
 const ListItems = (initialItems: string[] = []): ListItemCollection => {
   let items: string[] = initialItems;
-  console.log('ran constructor? ${items}', items);
-  
+  let previousItems: string[] = [];
+  console.log("ran constructor? ${items}", items);
+
   return {
     addItem: (item: string) => {
-      console.log('adding to ${items}',items)
+      console.log("adding to ${items}", items);
       items = items.concat([item]);
     },
     getItems: (): string[] => {
-      console.log(items); 
-      return items
+      console.log(items);
+      return items;
     },
-    removeItem: (itemIndexToRemove: number): removeItemResults => {
-      const undoList = [...items];
-      const newList = items.filter((x, index) => index !== itemIndexToRemove);
-      items = newList;
-      return { undoList, newList };
+    removeItem: (itemIndexToRemove: number) => {
+      previousItems = [...items];
+      items = items.filter((x, index) => index !== itemIndexToRemove);
+
+      //return newList };
+    },
+    undoRemoveLastItem: () => {
+      items = previousItems;
     }
   };
 };
